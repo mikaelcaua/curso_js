d9836193e24fd7061db2e3eae2d11bae53b26d51
mikaelcaua <mikaelcauasilva1@gmail.com>
2023-08-05 16:32:53 -0300

    Update script.js

--- ex11/script.js
+++ ex11/script.js
@@ -5,11 +5,11 @@ function adicionar(){
     let tvalores = window.document.getElementById('valores')
     n1 = Number(tn1.value)
     if (tn1.value == 0){
-        window.alert('Você precisa inserir um numero')
+        window.alert('digite um numero')
     }
     else{
         if(n1<0 || n1>100){
-            window.alert('insira um numero dentro do intervalo informado')
+            window.alert('insira um numero dentro do intevalo informado')
         }
         else{
             v.push(n1)
@@ -39,4 +39,11 @@ function finalizar(){
     }
     let tres = window.document.getElementById('res')    
     tres.innerHTML = `Ao todo temos ${v.length} numeros cadastrados <br></br> O maior valor informado foi ${max} <br></br> O menor valor informado foi ${min} <br></br> Somando todos os valores temos: ${sum} <br></br> A média dos valores é ${sum/v.length}`
+
+    // tres.innerHTML = ''
+    // tres.innerHTML = tres.innerHTML + `<p>Ao todo temos ${v.length} numeros cadastrados</p>`
+    // tres.innerHTML = tres.innerHTML + `<p>O maior valor informado foi ${max}</p>`
+    // tres.innerHTML = tres.innerHTML + `<p>O menor valor informado foi ${min}</p>` 
+    // tres.innerHTML = tres.innerHTML + `<p>Somando todos os valores temos: ${sum}</p>`
+    // tres.innerHTML = tres.innerHTML + `<p>A média dos valores é ${sum/v.length}</p>`
 }
